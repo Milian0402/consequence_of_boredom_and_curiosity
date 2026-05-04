50bcead306ed0a96422cd860de8f32f83c9c0a98
Milian0402 <max@nordler.se>
2026-05-05 01:24:07 +0200

    Extend AMX direct-B route to 832

--- src/gemm.c
+++ src/gemm.c
@@ -34,7 +34,7 @@ enum {
 };
 
 #ifndef COB_SGEMM_AMX_STRIDED_B_MAX_N
-#define COB_SGEMM_AMX_STRIDED_B_MAX_N 768
+#define COB_SGEMM_AMX_STRIDED_B_MAX_N 832
 #endif
 
 /* Direct source-B loads are slower for this power-of-two row stride; packed B avoids the conflict. */
